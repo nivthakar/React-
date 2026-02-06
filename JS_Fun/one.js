08de103bfb54bcb7b4755f11505d0bd42a85c681
nivthakar <niravthakar1992@gmail.com>
2026-02-06 18:40:31 +0530

    Week Data

---
+++ JS_Fun/one.js
@@ -0,0 +1,13 @@
+function demo(a,b){
+    return a+b;
+}
+
+console.log(demo(10,20));   
+
+// function demo (no){
+    
+//     for(let i=1; i<=no; i++){
+//         console.log(i);
+//     }
+// }
+// demo(5);
---
+++ JS_Function/fun_one.js
@@ -0,0 +1,55 @@
+const greet = (callback) => {
+    console.log("Hello!");
+    callback();
+};
+
+const sayBye = () => {
+    console.log("Good Bye!");
+};
+
+greet(sayBye);
+
+// const MYname =()=>{
+//     console.log("OKAY MIHIR");    
+// };
+//MYname();
+// (()=>{
+//     console.log("This is fun one Mihir");
+// })();
+
+// (()=>{
+//     console.log("This is fun one file");
+// })();
+
+// demo=(a,b)=>{
+//     console.log(a+b);       
+// }
+// demo(10,20);
+/*
+(function(){
+    console.log("This is fun one file");
+})();
+*/
+// demo =function(name,city){
+//     console.log("This is demo function");
+//     console.log("Name is "+name+" and city is "+city);
+// }
+
+// demo("John","New York");
+/*
+function sum(a,b){
+    return a+b;
+}
+sum(10,20);
+sum(10,20);
+*/
+// function fun(city="New Delhi"){
+//     console.log("Your city name is "+city);
+// }
+// fun("New York");
+// fun();
+// fun("Calliforniya");
+// fun("Last wages");
+// fun("New jursey");
+
+ console.log("This test");
